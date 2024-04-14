3dfcbc199982d5fa3795dc6dda1f6ab15f17063a
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-04-13 22:13:25 -0700

    Update run.sh script to fix directory paths and activate virtual environment

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
-cd /home/pimedia/mtv_websocker_server
+cd /home/pimedia/mtvwebsocket/
 
 if [ -n "$VIRUTAL_ENV" ]; then
+    cd ./mtv_websocket_server
     python3 mtvwebsocketserver.py
     echo "Venv activated\nStarting server"
 else
     source ./bin/activate
+    cd ./mtv_websocket_server
     python3 mtvwebsocketserver.py
     echo "Venv activated\nStarting server"
 fi
